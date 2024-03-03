bbed2e0d3562aa386e3727b97c2190c955d6dc39
data engineer <132142685+rajdeepUWE@users.noreply.github.com>
2024-03-03 18:10:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,8 @@ import yfinance as yf
 from datetime import datetime, timedelta
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_absolute_error, mean_squared_error
-from tensorflow.keras.models import load_model
 import plotly.graph_objs as go
 import requests
-import os
 
 # Function to calculate moving averages
 def calculate_moving_average(data, window_size):
@@ -20,12 +18,6 @@ def create_dataset(data, look_back=100):
     for i in range(len(data) - look_back):
         X.append(data[i:(i + look_back)])
     return np.array(X)
-
-# Function to download model file
-def download_model(model_url, model_filename):
-    response = requests.get(model_url)
-    with open(model_filename, 'wb') as f:
-        f.write(response.content)
 
 # Streamlit app
 def main():
@@ -40,7 +32,7 @@ def main():
     end_date = st.sidebar.date_input('Select End Date:', datetime.now())
 
     # Model selection
-    selected_model = st.sidebar.radio("Select Model", ("Neural Network", "Random Forest", "Linear Regression", "LSTM"))
+    selected_model = st.sidebar.radio("Select Model", ("Neural Network", "Random Forest"))
 
     # Load stock data
     if stock_symbol:
@@ -76,18 +68,12 @@ def main():
             elif selected_model == "Random Forest":
                 model_url = "https://github.com/rajdeepUWE/stock_market_forecast/raw/master/random_forest_model.h5"
                 model_filename = "random_forest_model.h5"
-            elif selected_model == "Linear Regression":
-                model_url = "https://github.com/rajdeepUWE/stock_market_forecast/raw/master/linear_regression_model.h5"
-                model_filename = "linear_regression_model.h5"
-            elif selected_model == "LSTM":
-                model_url = "https://github.com/rajdeepUWE/stock_market_forecast/raw/master/LSTM.h5"
-                model_filename = "LSTM.h5"
 
             # Download model file
-            download_model(model_url, model_filename)
+            response = requests.get(model_url)
 
             # Load model
-            model = load_model(model_filename)
+            model = response.content
 
             # Scale data
             scaler = MinMaxScaler(feature_range=(0, 1))
